ed2fa6d0e80aa5595fcad7719242f3d80631b950
Martin <martin.dobrew@outlook.com>
2024-07-07 00:19:08 +0200

    gut layout for main page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 const app = require('./app');
-const port = 3030;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
 });
