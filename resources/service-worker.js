29af38278e08038e18aa68af10c7b2966c17cabd
Martin <martin.dobrew@outlook.com>
2024-08-02 21:07:51 +0300

    testing

--- resources/service-worker.js
+++ resources/service-worker.js
@@ -1,8 +1,11 @@
+const { test } = require("media-typer");
+
 self.addEventListener('push', function(event) {
     //This extracts the payload data from the push event. The data is assumed to be in JSON format.
     // fields: title, body, url
     const data = event.data.json();
-    const options = {body: data.body};
+    console.log(data);
+    const options = {body: data.body, test: 'test'};
 
     event.waitUntil(self.registration.showNotification(data.title, options));
 
@@ -25,6 +28,6 @@ self.addEventListener('notificationclick', function(event) {
     console.log(event.notification);
     console.log(event.data);
     console.log(event.notification.data);
-    const data = event.data.json();
+    const data = event.notification.data.json();
     event.waitUntil(clients.openWindow("https://someurl"+data.data.test));
 });
